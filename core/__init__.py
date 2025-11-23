b41b8fe43968b70913547c8fcf22b11901acbd03
bangdani359-bit <bangdani359@gmail.com>
2025-11-23 04:43:43 -0800

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,9 @@
-from .logger      import Log
-from .runtime     import Run, Utils
-from .parser      import Parser
-from .xctid       import Signature
-from .anon        import Anon
-from .grok        import Grok as WormAi
+from .logger  import Log
+from .runtime import Run, Utils
+from .parser  import Parser
+from .xctid   import Signature
+from .anon    import Anon
+
+# expose Grok secara benar
+from .grok    import Grok
+from .grok    import Grok as WormAi
